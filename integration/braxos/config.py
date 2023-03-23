9594ad3474e1c4ca84d6bcc6c51c6240acec613b
Mykhailo Makovetskiy <mmakovetskiy@deloitte.com>
2023-03-23 15:03:59 +0000

    JBB-60 Braxos
    Updated Braxos to follow general flow in connector

--- integration/braxos/config.py
+++ integration/braxos/config.py
@@ -1,40 +1,10 @@
 """Braxos Configuration of Integration."""
 from dataclasses import dataclass, field
-from typing import List, Optional, Union
+from typing import List, Union
 
-from integration.base_integration import StorageInfo
 from paramiko.rsakey import RSAKey
 
-# TODO: Should be moved to base config in future
-@dataclass
-class FetchStrategy:
-    """Fetch starategy"""
-
-    type: str = ""
-    description: str = ""
-
-
-# TODO: Should be moved to base config in future
-@dataclass
-class ExtraInfo:
-    """Extra config parameters"""
-
-    raw: StorageInfo = field(default_factory=StorageInfo)
-    participant_id: int = -1
-    timezone: str = ""
-    fetch_strategy: FetchStrategy = field(default_factory=FetchStrategy)
-
-
-# TODO: Should be moved to base config in future
-@dataclass
-class MeterCfg:
-    """Meter config representation"""
-
-    meter_name: str = ""
-    meter_id: int = -1
-    meter_uri: str = ""
-    type: str = ""
-    standardized: StorageInfo = field(default_factory=StorageInfo)
+from integration.base_integration import ExtraInfo, MeterCfg, TimeShift
 
 
 @dataclass
@@ -49,13 +19,4 @@ class BraxosCfg:  # pylint:disable=too-many-instance-attributes
     server: str = ""
     username: str = ""
     gap_regeneration_window: int = -1
-
-
-@dataclass
-class FetchPayload:
-    """Fetch request hour data"""
-
-    meter_cfg: Optional[MeterCfg] = None
-    file_name: Optional[str] = ""
-    start_date: Optional[str] = ""
-    end_date: Optional[str] = ""
+    timestamp_shift: TimeShift = field(default_factory=TimeShift)
--- integration/braxos/connector.py
+++ integration/braxos/connector.py
@@ -1,20 +1,17 @@
 """Braxos Integration module"""
 import base64
 import uuid
-from collections import Counter
 from io import StringIO
-from json import JSONDecodeError, dumps, load
+from json import dumps, load
 from queue import Queue
 from typing import Optional
+
 import paramiko
-from dataclass_factory import Factory
-from expiringdict import ExpiringDict
 from common import settings as CFG
 from common.elapsed_time import elapsed_timer
 from common.logging import Logger
 from integration.base_integration import BasePullConnector
 from integration.braxos.config import BraxosCfg
-from integration.braxos.exception import MalformedConfig
 from integration.braxos.workers import (
     FetchWorker,
     GapsDetectionWorker,
@@ -31,59 +28,23 @@ class BraxosConnector(BasePullConnector):
 
     def __init__(self, env_tz_info):
         super().__init__(env_tz_info=env_tz_info)
-        self._factory = Factory()
-
-        self._missed_hours = ExpiringDict(max_len=2000, max_age_seconds=3600)
         self._config: Optional[BraxosCfg] = None
         self._gaps_worker: Optional[GapsDetectionWorker] = None
-
-        # TODO: SHOULD be moved to the BasePullConnector and propagated to
-        # the other pull integrations
-
-        self._fetched_files_q: Queue = Queue()
-        self._fetch_update_q: Queue = Queue()
         self._fetch_worker: Optional[FetchWorker] = None
-
-        self._standardized_files: Queue = Queue()
-        self._standardized_update_files: Queue = Queue()
-        self._standardized_files_count: Counter = Counter()
         self._standardize_worker: Optional[StandardizeWorker] = None
+
+        # TODO: Should be removed  after Coned moving to the new approach
+        # and final refactoring
+        self._standardized_files: Queue = Queue()
 
     def configure(self, data: bytes) -> None:
         self._logger.debug("Loading configuration.")
         with elapsed_timer() as elapsed:
-            try:
-                js_config = self._before_configuration(data)
-                if not js_config:
-                    raise MalformedConfig("Recieved Malformed configuration JSON")
-
-                self._config = self._factory.load(js_config, BraxosCfg)
-                raw_key = base64.b64decode(self._config.key.encode("utf-8")).decode(
-                    "utf-8"
-                )
-
-                self._config.key = paramiko.RSAKey.from_private_key(
-                    StringIO(raw_key[1:-1])
-                )
-            except (ValueError, TypeError, JSONDecodeError) as err:
-                raise MalformedConfig from err
-
-            self._gaps_worker = GapsDetectionWorker(
-                missed_hours_cache=self._missed_hours, config=self._config
-            )
-
-            self._fetch_worker = FetchWorker(
-                missed_hours=self._missed_hours,
-                fetched_files=self._fetched_files_q,
-                fetch_update=self._fetch_update_q,
-                config=self._config,
-            )
-
-            self._standardize_worker = StandardizeWorker(
-                raw_files=self._fetched_files_q,
-                standardized_files=self._standardized_files,
-                standardize_update=self._standardized_update_files,
-                config=self._config,
+            self._config = self._config_factory(data, BraxosCfg)
+            self._configure_workers(
+                gaps_cls=GapsDetectionWorker,
+                fetch_cls=FetchWorker,
+                standardize_cls=StandardizeWorker,
             )
 
         self._logger.debug(
@@ -95,65 +56,12 @@ class BraxosConnector(BasePullConnector):
             },
         )
 
-    # TODO: @todo After comletion check posibility move to some base class
-    def get_missed_hours(self) -> None:
-        """Get list of missed hours"""
-        # Standardized data stored separately by type meter. It means that it
-        # possible to miss different hours for each meter in a list.
-        # As the result we need to check each given meter for misssed hour
-        # and build index (relation) between missedd hour and related meters
-
-        self._logger.info("Matching missed hour.")
-
-        with elapsed_timer() as elapsed:
-            self._gaps_worker.run(self._run_time)
-
-        self._logger.debug(
-            "Matched missed hour.", extra={"labels": {"elapsed_time": elapsed()}}
-        )
-
-    # TODO: @todo Redesign. Move to base class
-    def fetch(self) -> None:
-        with elapsed_timer() as ellapsed:
-            self._logger.info("Fetching data.")
-            self._fetch_data()
-            self._logger.debug(
-                "Fetched missed hours.", extra={"labels": {"elapsed_time": ellapsed()}}
-            )
-
-    def _fetch_data(self) -> None:
-        """Integration Fetch logic"""
-        self._logger.info(f"Fetching `{self.__name__}` data")
-
-        if self._fetch_worker is None:
-            self._logger.error("The 'configure' method must be run before. Complete.")
-            return None
-
-        self._fetch_worker.run(self._run_time)
-        return None
-
-    # TODO: @todo Candidate to be in a base class
-    def standardize(self) -> None:
-        with elapsed_timer() as elapsed:
-            self._logger.info("Start standardizing of fetched data.")
-            if self._standardize_worker is None:
-                self._logger.error(
-                    "The 'configure' method must be run before. Complete."
-                )
-                return None
-            self._standardize_worker.run(self._run_time)
-
-            self._logger.info(
-                "Completed data standardization.",
-                extra={"labels": {"elapsed_time": elapsed()}},
-            )
-        return None
-
-    def run(self):
-        super().run()
-        self.get_missed_hours()
-        self.fetch()
-        self.standardize()
+    def _after_configuration(self, config: BraxosCfg, *args, **kwargs) -> BraxosCfg:
+        """Actions before config parser"""
+        super()._after_configuration(config, *args, **kwargs)
+        raw_key = base64.b64decode(config.key.encode("utf-8")).decode("utf-8")
+        config.key = paramiko.RSAKey.from_private_key(StringIO(raw_key[1:-1]))
+        return config
 
 
 def main(event, context):  # pylint:disable=unused-argument
